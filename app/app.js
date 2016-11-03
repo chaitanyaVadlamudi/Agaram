8516411ead0b6aea647137367e9e188fe25bebc8
chaitanyaVadlamudi <nagachaitanya1994@gmail.com>
2016-11-03 20:43:57 +0530

    added agaram

---
+++ app/app.js
@@ -0,0 +1,31 @@
+(function(){
+    'use strict'
+angular.module("agaramApp",["home","student","donar","volunteer","header","ui.router"]);
+  angular.module("agaramApp")
+     .config([ "$stateProvider",function($stateProvider){
+         var basePath ="app/templates/"
+         var homeObj={
+             templateUrl:basePath+"home.html"
+         };
+
+          var studentObj={
+             templateUrl:basePath+"student.html"
+         };
+         var volunteerObj={
+             templateUrl:basePath+"volunteer.html"
+         };
+         var aboutObj={
+             templateUrl:basePath+"about.html"
+         };
+         var contactObj={
+             templateUrl:basePath+"contact.html"
+         };
+         
+         $stateProvider.state("home",homeObj);
+         $stateProvider.state("student",studentObj);
+         $stateProvider.state("volunteer",volunteerObj);
+         $stateProvider.state("about",aboutObj);
+         $stateProvider.state("contact",contactObj);
+     }]);
+
+    })();
